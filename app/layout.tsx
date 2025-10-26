375c69b6de7f86fdfe989d13a2e96c50819aad80
praneet1503 <praneetnrana@gmail.com>
2025-10-26 13:54:37 +0400

    Add Speed Insights integration and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 import ThemeProvider from './components/ThemeProvider'
 
@@ -40,6 +41,7 @@ export default function RootLayout({
         <ThemeProvider>
           {children}
           <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
